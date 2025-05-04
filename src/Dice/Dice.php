678837c98d4ac1b214812aa60fa714adb4f51615
Max Lindgren <malw24@student.bth.se>
2025-05-05 01:19:28 +0200

    Done with all the exercises for this week

--- src/Dice/Dice.php
+++ src/Dice/Dice.php
@@ -1,24 +1,48 @@
 <?php
 
+// namespace App\Dice;
+
+// class Dice
+// {
+//     protected $value;
+
+//     public function __construct()
+//     {
+//         $this->value = null;
+//     }
+
+//     public function roll(): int
+//     {
+//         $this->value = random_int(1, 6);
+//         return $this->value;
+//     }
+
+//     public function getValue(): int
+//     {
+//         return $this->value;
+//     }
+
+//     public function getAsString(): string
+//     {
+//         return "[{$this->value}]";
+//     }
+
+// }
+
 namespace App\Dice;
 
 class Dice
 {
-    protected $value;
+    protected int $value;
 
     public function __construct()
     {
-        $this->value = null;
+        $this->value = random_int(1, 6);
     }
 
     public function roll(): int
     {
         $this->value = random_int(1, 6);
-        return $this->value;
-    }
-
-    public function getValue(): int
-    {
         return $this->value;
     }
 
